9f17d870c0b6c82d11083a96321b9b5ab9b669ab
Justin Mapanao <justin.mapanao@epfl.ch>
2022-12-03 23:05:52 +0100

    Added link modifications and job_title

--- pages/api/job-descriptions/index.js
+++ pages/api/job-descriptions/index.js
@@ -1,15 +1,25 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-
-let url = 'https://www.linkedin.com/jobs/view/senior-software-engineer-linux-distribution-at-cysec-3111676795/?originalSubdomain=ch'
-axios(url)
+const { find } = require('domutils');
+const base_url = 'https://www.linkedin.com/jobs/view/'
+let user_url = 'https://www.linkedin.com/jobs/collections/recommended/?currentJobId=3334551559'
+let JobId = user_url.split("currentJobId=")[1];
+let final_url = ''
+if (typeof JobId === 'string' && JobId.length === 0) {
+    final_url = user_url
+}
+else {
+    final_url = base_url+JobId
+}
+axios(final_url)
 .then (response => {
 const html = response.data;
 const $ = cheerio.load(html);
-const text = $('div.show-more-less-html__markup.show-more-less-html__markup--clamp-after-5')
-let Text = ''
-text.each((index, element) => {
-    Text += $(element).find('p').text().trim()
+const job_title=$('h1').text().trim()
+// const company = $('span').text().trim()
+const job_details = $("div.show-more-less-html__markup.show-more-less-html__markup--clamp-after-5")
+let description = ''
+job_details.children().each((index, element) => {
+    description += ($(element).text().trim() + '\n')
 });
-console.log(Text)
 })
